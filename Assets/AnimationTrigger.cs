7485e01d20bf6384378d58a6fa2d1091a1585be6
Shinan Liu <Shinan.liu@yahoo.com>
2020-12-27 00:47:35 -0500

    free look cmaera

---
+++ Assets/AnimationTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator _anim;
+    public ParticleSystem particle;
+    public bool stop;
+    private float stopTimer;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        particle = GetComponentInChildren<ParticleSystem>();
+        stop = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stop == true)
+        {
+            stopTimer += Time.deltaTime;
+            Debug.Log(stopTimer + " seconds");
+
+            if(stopTimer >= 0.7)
+            {
+                particle.Stop();
+   
+                
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            Debug.Log("collided player");
+            _anim.SetTrigger("Startled");
+            particle.Play();
+            stop = true;
+            
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            stop = false;
+            stopTimer = 0;
+    }
+}
